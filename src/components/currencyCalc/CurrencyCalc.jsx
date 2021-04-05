6b366b16529b301ff2d9ed8eda8a56d10453adcb
olayinka <olayinka.omosehin@charisol.io>
2021-04-05 22:33:40 +0100

    fixed institutionId bug

--- src/components/currencyCalc/CurrencyCalc.jsx
+++ src/components/currencyCalc/CurrencyCalc.jsx
@@ -2,6 +2,7 @@ import React, { useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import "./CurrencyCalc.scss";
 
+import UserContext from "../../context/user/userContext";
 import PaymentContext from "../../context/payment/paymentContext";
 
 import { Menu, Dropdown } from "antd";
@@ -9,9 +10,10 @@ import { Menu, Dropdown } from "antd";
 const CurrencyCalc = () => {
   const [tempValue, setTempValue] = useState("");
   const [tempCountry, setTempCountry] = useState(false);
-  const path = window.location.pathname;
-  
-  const userData = JSON.parse(localStorage.getItem("user"));
+
+  const userContext = useContext(UserContext);
+
+  const userData = userContext.state.user;
   const regularUserData = userData ? userData.regularAccountDetail : null;
   const countryId = regularUserData ? regularUserData.countryId : ``;
   const paymentContext = useContext(PaymentContext);
@@ -25,9 +27,9 @@ const CurrencyCalc = () => {
     fee,
     rate,
     receiveType,
-    reverse,
+    reverse
   } = paymentContext.state.fxDetails;
-  
+
   const userCurrency = paymentContext.state.countries.find(
     country => country.id === countryId
   );
@@ -39,18 +41,18 @@ const CurrencyCalc = () => {
   const handleClick = (e, name) => {
     const data = {
       sendCurrency: name === "sendCurrency" ? e.key : sendCurrency,
-      destinationCurrency: name === "destinationCurrency" ? e.key : destinationCurrency,
+      destinationCurrency:
+        name === "destinationCurrency" ? e.key : destinationCurrency,
       baseAmount,
       convertedAmount: baseAmount === "" ? "" : convertedAmount,
       receiveType,
-      reverse: reverse,
+      reverse: reverse
     };
     setTempCountry(true);
     paymentContext.getFxRates(data);
   };
 
-  const handleChange = (e) => {
-
+  const handleChange = e => {
     if (e.target.name === "convertedAmount") {
       paymentContext.setReverseCalc(true);
       sendFxRateRequest(e, true);
@@ -59,7 +61,7 @@ const CurrencyCalc = () => {
       sendFxRateRequest(e, false);
     }
 
-    setTempValue(e.target.value)
+    setTempValue(e.target.value);
   };
 
   const sendFxRateRequest = (e, calcType) => {
@@ -73,7 +75,7 @@ const CurrencyCalc = () => {
       fee: baseAmount === "" ? 0 : fee,
       rate: baseAmount === "" ? 0 : rate,
       receiveType: receiveType,
-      reverse: calcType,
+      reverse: calcType
     };
 
     paymentContext.getFxRates(data);
@@ -82,19 +84,19 @@ const CurrencyCalc = () => {
   const addFlagsToCountries = () => {
     const countries = paymentContext.state.countries;
 
-    const withFlags = countries.map((ct) => {
+    const withFlags = countries.map(ct => {
       const flagCode = ct.currencyCode.slice(0, 2).toLowerCase();
 
-      if (flagCode === 'xa') {
+      if (flagCode === "xa") {
         return {
           ...ct,
-          flag: `https://www.countryflags.io/cg/flat/24.png`,
-        }
+          flag: `https://www.countryflags.io/cg/flat/24.png`
+        };
       }
 
       return {
         ...ct,
-        flag: `https://www.countryflags.io/${flagCode}/flat/24.png`,
+        flag: `https://www.countryflags.io/${flagCode}/flat/24.png`
       };
     });
 
@@ -104,11 +106,8 @@ const CurrencyCalc = () => {
   const currencies = addFlagsToCountries();
 
   const sendCurrencyMenu = (
-    <Menu
-      onClick={(e) => handleClick(e, "sendCurrency")}
-      name='sendCurrency'      
-    >
-      {currencies.map((currency) => (
+    <Menu onClick={e => handleClick(e, "sendCurrency")} name="sendCurrency">
+      {currencies.map(currency => (
         <Menu.Item key={currency.currencyCode}>
           <img src={currency.flag} alt={currency.currencyCode} />{" "}
           {currency.currencyCode}
@@ -119,10 +118,10 @@ const CurrencyCalc = () => {
 
   const receiveCurrencyMenu = (
     <Menu
-      onClick={(e) => handleClick(e, "destinationCurrency")}
+      onClick={e => handleClick(e, "destinationCurrency")}
       selectedKeys={destinationCurrency}
     >
-      {currencies.map((currency) => (
+      {currencies.map(currency => (
         <Menu.Item key={currency.currencyCode}>
           <img src={currency.flag} alt={currency.currencyCode} />{" "}
           {currency.currencyCode}
@@ -132,15 +131,15 @@ const CurrencyCalc = () => {
   );
 
   return (
-    <div id='currency-calculator'>
+    <div id="currency-calculator">
       <form>
         <label>
-          <div className='currency-input'>
+          <div className="currency-input">
             <small>You Send</small>
             <input
-              type='number'
-              name='baseAmount'
-              placeholder='1,000'
+              type="number"
+              name="baseAmount"
+              placeholder="1,000"
               value={reverse === false ? tempValue : baseAmount}
               onChange={handleChange}
               onKeyUp={handleChange}
@@ -148,7 +147,7 @@ const CurrencyCalc = () => {
           </div>
 
           <Dropdown overlay={sendCurrencyMenu}>
-            <p className='currency-dropdown'>
+            <p className="currency-dropdown">
               <img
                 src={`https://www.countryflags.io/${sendCurrency
                   .slice(0, 2)
@@ -156,44 +155,44 @@ const CurrencyCalc = () => {
                 alt={sendCurrency}
               />
               {sendCurrency}
-              <span className='material-icons'>arrow_drop_down</span>
+              <span className="material-icons">arrow_drop_down</span>
             </p>
           </Dropdown>
         </label>
 
-        <div className='logic-box'>
+        <div className="logic-box">
           <p>
-            <span className='logic__symbols'>
+            <span className="logic__symbols">
               <small>–</small>
             </span>{" "}
             {fee} {sendCurrency}
-            <span className='logic__description'>Fee (Including IOF)</span>
+            <span className="logic__description">Fee (Including IOF)</span>
           </p>
           <p>
-            <span className='logic__symbols'>
+            <span className="logic__symbols">
               <small>=</small>
             </span>{" "}
             {actualAmount} {sendCurrency}
-            <span className='logic__description'>Amount we'll convert</span>
+            <span className="logic__description">Amount we'll convert</span>
           </p>
           <p>
-            <span className='logic__symbols'>
+            <span className="logic__symbols">
               <small>÷</small>
             </span>{" "}
             {rate}
-            <span className='logic__description'>
+            <span className="logic__description">
               Commercial rate (144 hrs)
             </span>
           </p>
         </div>
 
         <label>
-          <div className='currency-input'>
+          <div className="currency-input">
             <small>They Receive</small>
             <input
-              type='number'
-              name='convertedAmount'
-              placeholder='3,900'
+              type="number"
+              name="convertedAmount"
+              placeholder="3,900"
               value={reverse === true ? tempValue : convertedAmount}
               onChange={handleChange}
               onKeyUp={handleChange}
@@ -202,7 +201,7 @@ const CurrencyCalc = () => {
           </div>
 
           <Dropdown overlay={receiveCurrencyMenu}>
-            <p className='currency-dropdown'>
+            <p className="currency-dropdown">
               <img
                 src={`https://www.countryflags.io/${destinationCurrency
                   .slice(0, 2)
@@ -210,18 +209,18 @@ const CurrencyCalc = () => {
                 alt={destinationCurrency}
               />
               {destinationCurrency}
-              <span className='material-icons'>arrow_drop_down</span>
+              <span className="material-icons">arrow_drop_down</span>
             </p>
           </Dropdown>
         </label>
 
-        <div className='notice'>
+        <div className="notice">
           <p>This page is refreshed every 60 seconds</p>
         </div>
 
-        <div className='form__submit'>
-          <Link to='/payment/recipient'>
-            <button type='button' className='form__submit__button'>
+        <div className="form__submit">
+          <Link to="/payment/recipient">
+            <button type="button" className="form__submit__button">
               Send this Amount
             </button>
           </Link>
--- src/pages/paymentOptions/PaymentOptions.jsx
+++ src/pages/paymentOptions/PaymentOptions.jsx
@@ -9,15 +9,20 @@ import CustomCheckbox from "../../components/customCheckbox/CustomCheckbox";
 import PaymentSummaryCard from "../../components/paymentSummaryCard/PaymentSummaryCard";
 
 import PaymentContext from "../../context/payment/paymentContext";
+import UserContext from "../../context/user/userContext";
 
 function PaymentOptions({ showTips }) {
   const paymentContext = useContext(PaymentContext);
-
+  const userContext = useContext(UserContext);
   const selected = paymentContext.state.paymentOption;
   const recipient = paymentContext.state.recipient;
   const institution = paymentContext.state.institution;
   const transactionType = paymentContext.state.transactionType;
   const fx = paymentContext.state.fxDetails;
+  console.log(userContext);
+  const institutionId = userContext.state.user.studentAccountDetail
+    ? userContext.state.user.studentAccountDetail.institutionId
+    : "";
 
   const [summary, setSummary] = useState({
     sendAmount: fx.baseAmount,
@@ -26,7 +31,7 @@ function PaymentOptions({ showTips }) {
     fees: "Free",
     convertedAmount: fx.convertedAmount,
     destinationCurrency: fx.destinationCurrency,
-    receivingMethod: selected,
+    receivingMethod: selected
   });
 
   const [redirect, setRedirect] = useState(false);
@@ -37,47 +42,50 @@ function PaymentOptions({ showTips }) {
       key: 1,
       title: "E-transfer",
       speed: "1 hour",
-      cost: "Free",
+      cost: "Free"
     },
     {
       key: 2,
       title: "Bank payment",
       speed: "1 hour",
-      cost: "Free",
+      cost: "Free"
     },
     {
       key: 3,
       title: "Debit card",
       speed: "1 hour",
-      cost: fx.baseAmount * 0.01,
+      cost: fx.baseAmount * 0.01
     },
     {
       key: 4,
       title: "Credit card",
       speed: "1 hour",
-      cost: fx.baseAmount * 0.01,
-    },
+      cost: fx.baseAmount * 0.01
+    }
   ];
 
-  const handleClick = (card) => {
+  const handleClick = card => {
     paymentContext.setPaymentOption(card.title);
 
     setSummary({
       ...summary,
-      fees: card.cost,
+      fees: card.cost
     });
   };
 
   const initiateTransaction = () => {
     const transactionDetails = {
-      recipientId: paymentContext.state.transactionType === "Individual" ? recipient.id : institution.institutionId,
+      recipientId:
+        paymentContext.state.transactionType === "Individual"
+          ? recipient.id
+          : institutionId,
       userId: localStorage.getItem("userId"),
       bankInfoId: recipient.bankInfo[0].id,
       sendCurrency: fx.sendCurrency,
       destinationCurrency: fx.destinationCurrency,
       baseAmount: fx.baseAmount,
       transactionType: paymentContext.state.transactionType,
-      receiveType: fx.receiveType,
+      receiveType: fx.receiveType
     };
 
     setProgress("4");
@@ -98,7 +106,7 @@ function PaymentOptions({ showTips }) {
               <p>Select the payment options</p>
             </div>
 
-            {paymentMethods.slice(0, 2).map((card) => (
+            {paymentMethods.slice(0, 2).map(card => (
               <div
                 className="shadow-box"
                 key={card.key}
