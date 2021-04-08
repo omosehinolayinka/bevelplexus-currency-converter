c5380edb2c0677fe04e42f3808bf860f5035bb0e
olayinka <olayinka.omosehin@charisol.io>
2021-04-08 17:43:47 +0100

    Fixed misc-bugs

--- src/pages/dashboard/LastTransaction.jsx
+++ src/pages/dashboard/LastTransaction.jsx
@@ -2,37 +2,41 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 
 import PaymentContext from "../../context/payment/paymentContext";
-import RecipientContext from '../../context/recipients/recipientContext'
+import RecipientContext from "../../context/recipients/recipientContext";
 
-function LastTransaction({ data }) {
+function LastTransaction({ data, institution }) {
   const paymentContext = useContext(PaymentContext);
-  const recipientContext = useContext(RecipientContext)
+  const recipientContext = useContext(RecipientContext);
 
   const selectRecipient = () => {
-    const id = data[0].recipient.id
-    paymentContext.setCurrentRecipient(data[0])
+    const id = data[0].recipient.id;
+    paymentContext.setCurrentRecipient(data[0]);
     recipientContext.getRecipient(id, paymentContext);
   };
-
   return (
-    <div className='box-one__content'>
-      <div className='user-details'>
-        <div className='user-details__avi'>
-          <img src='./assets/img/avatar-square.png' alt='avi' />
+    <div className="box-one__content">
+      <div className="user-details">
+        <div className="user-details__avi">
+          <img src="./assets/img/avatar-square.png" alt="avi" />
           {/* <img src="/assets/svg/brazil-flag.svg" alt="" className="user-details__avi__flag"/> */}
         </div>
-        <span className='box-one__text-wrapper'>
-          <h3> {data[0].recipient.name} </h3>
-          <p> {data[0].recipient.email} </p>
+        <span className="box-one__text-wrapper">
+          <h3>
+            {" "}
+            {data.transactionType === "Individual"
+              ? data[0]?.recipient?.name
+              : institution?.name}{" "}
+          </h3>
+          <p> {data.transactionType === "Individual" ? data[0]?.recipient?.email : ""} </p>
         </span>
       </div>
 
-      <div className='box-one__text-wrapper alt'>
+      <div className="box-one__text-wrapper alt">
         <h3>
           <span>
             {data[0].baseAmount.toLocaleString()} {data[0].sendCurrency}
           </span>
-          <span className='material-icons'>arrow_right</span>
+          <span className="material-icons">arrow_right</span>
           <span>
             {data[0].convertedAmount.toLocaleString()} {data[0].destinationCurrency}
           </span>
@@ -40,8 +44,8 @@ function LastTransaction({ data }) {
         <p>Last Transaction</p>
       </div>
 
-      <div className='box-one__cta'>
-        <Link to='/payment/transfer' onClick={selectRecipient} >
+      <div className="box-one__cta">
+        <Link to="/payment/transfer" onClick={selectRecipient}>
           <button>Send Money</button>
         </Link>
       </div>
